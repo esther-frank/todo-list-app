1b2ccc1b556d79bdb31979ff71d6f25457a9d6e3
Esther Frank <185348393+esther-frank@users.noreply.github.com>
2024-10-31 13:06:46 +0000

    tasks can be set as complete or incomplete

--- src/app/todoListSlice.ts
+++ src/app/todoListSlice.ts
@@ -1,5 +1,6 @@
 import { createSlice } from '@reduxjs/toolkit'
 import { PayloadAction } from '@reduxjs/toolkit'
+import TodoList from '../components/todoList/TodoList'
 
 export interface TodoItem {
   id: number
@@ -21,10 +22,14 @@ export const todoSlice = createSlice({
   reducers: {
     addTodo: (state, action: PayloadAction<TodoItem>) => {
       state.items.push(action.payload)
+    },
+    setComplete: (state, action: PayloadAction<number>) => {
+      const task = state.items.findIndex(item => item.id === action.payload)
+      state.items[task].complete = !state.items[task].complete
     }
   }
 })
 
-export const { addTodo } = todoSlice.actions
+export const { addTodo, setComplete } = todoSlice.actions
 
 export default todoSlice.reducer
--- src/components/taskCard/TaskCard.tsx
+++ src/components/taskCard/TaskCard.tsx
@@ -1,16 +1,26 @@
 import React from 'react'
 import './TaskCard.css'
 
+import { useAppDispatch } from '../../app/hooks'
+
+import { setComplete } from '../../app/todoListSlice'
+
 interface TodoItem {
   id: number
   title: string
   complete: boolean
 }
 
-export default function TaskCard({ id, title, complete = false }: TodoItem) {
+export default function TaskCard({ id, title, complete }: TodoItem) {
+  const dispatch = useAppDispatch()
+
+  const handleChange = () => {
+    dispatch(setComplete(id))
+  }
+
   return (
     <div className="task-card" id={`${id}`}>
-      <input type="checkbox" checked={complete} />
+      <input type="checkbox" checked={complete} onChange={handleChange} />
       <p>{title}</p>
     </div>
   )
