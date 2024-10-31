a3367760245251dc7a73044f8c25ee217ea292d2
Esther Frank <185348393+esther-frank@users.noreply.github.com>
2024-10-31 10:49:05 +0000

    button adds an item to todo list

--- src/components/InputForm.tsx
+++ src/components/InputForm.tsx
@@ -1,5 +1,15 @@
 import React from 'react'
 
+import { useAppDispatch } from '../app/hooks'
+
+import { addTodo } from '../app/todoListSlice'
+
 export default function InputForm() {
-  return <button>Click to add another item</button>
+  const dispatch = useAppDispatch()
+
+  const handleClick = () => {
+    dispatch(addTodo({ id: 2, title: 'another item', complete: false }))
+  }
+
+  return <button onClick={handleClick}>Click to add another item</button>
 }
--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -2,11 +2,8 @@ import React from 'react'
 
 import { useAppSelector } from '../app/hooks'
 
-// import { addTodo } from '../app/todoListSlice'
-
 export default function TodoList() {
   const todoList = useAppSelector(state => state.todoList)
-  //   const dispatch = useAppDispatch()
 
   return (
     <ul>
